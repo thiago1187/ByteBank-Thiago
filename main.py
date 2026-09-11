c4c28b1f5eb5bd5576cb910c36caa26ef8b22357
Thiago Alves <thiago.amacena2@hotmail.com>
2026-09-10 21:17:43 -0300

    feat: implementa nivel 3 - fila de boletos agendados

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import contas
 import operacoes
 import extrato
+import boletos
 
 
 def menu_conta(conta):
@@ -13,7 +14,10 @@ def menu_conta(conta):
         print("4 - Pix")
         print("5 - Ver extrato")
         print("6 - Estornar ultima transacao")
-        print("7 - Voltar")
+        print("7 - Agendar boleto")
+        print("8 - Ver fila de boletos")
+        print("9 - Liquidar pagamentos")
+        print("10 - Voltar")
         opcao = input("Escolha uma opcao: ")
         if opcao == "1":
             contas.mostrar_saldo(conta)
@@ -28,6 +32,12 @@ def menu_conta(conta):
         elif opcao == "6":
             extrato.estornar(conta)
         elif opcao == "7":
+            boletos.agendar_boleto(conta)
+        elif opcao == "8":
+            boletos.ver_fila(conta)
+        elif opcao == "9":
+            boletos.liquidar_pagamentos(conta)
+        elif opcao == "10":
             break
         else:
             print("Opcao invalida.")
